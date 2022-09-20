114121067e97d4ae379562015c0ad1f59f512eba
septheazisca <septheaziscaaurora@gmail.com>
2022-09-20 15:59:27 +0700

    koneksi

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php
+  $host = "localhost"; 
+  $user = "root"; 
+  $password = ""; 
+  $database = "db_starbhakpustaka"; 
+
+  $connect = mysqli_connect($host,$user,$password,$database) or die ("GAGAL MENGHUBUNGKAN");
+?>
