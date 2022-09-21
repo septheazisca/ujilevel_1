100a6fb1225036b578f9bf7405ff4a31696c743c
septheazisca <septheaziscaaurora@gmail.com>
2022-09-21 11:49:36 +0700

    nav, sidebar, table datamurid

--- datamurid.php
+++ datamurid.php
@@ -9,7 +9,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="style.css">
+  <style>
+    <?php
+    include 'data-murid.css';
+    ?>
+  </style>
   <!-- icone -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
@@ -24,11 +28,14 @@
     <!-- ini nav -->
     <nav>
     <div class="logo">
+      <i class="fa-solid fa-book-open-reader"></i>
       <p>STARBHAKPUSTAKA</p>
     </div>
 
     <div class="profil">
-      <i class="fa-solid fa-user"></i>
+      <p class="hi">Hai, my name is...</p>
+      <i class="fa-solid fa-circle-user"></i>
+      <i class="fa-solid fa-bell"></i>
     </div>
   </nav>
 
@@ -41,9 +48,13 @@
       <ul>
         <li><a href="">Dashboard</a></li>
         <li><a href="pendataanmurid.html">Members Registration</a></li>
-        <li><a href="datamurid.php">Members Date Tabel</a></li>
+        <div class="lebel-datamurid">
+          <li><a href="datamurid.php">Members Date</a></li>
+        </div>
         <li><a href="pendataanbuku.html">Book Registration</a></li>
-        <li><a href="databuku.php">Book Data Table</a></li>
+        <div class="lebel-databuku">
+        <li><a href="databuku.php">Book Data</a></li>
+        </div>
         <li><a href="">About</a></li>
         <li><a href="">Contact</a></li>
       </ul>
@@ -52,9 +63,9 @@
 
   <div class="kotak-kanan">
     <p>Data Murid</p>
-
-      <h4><a href="pendataanmurid.html">[+] Tambah Baru</a></h4>
-      <table class="tbl-datamurid" border="1">
+    <div class="garis"></div>
+      <h4><a href="pendataanmurid.html">add new<i class="fa-solid fa-plus"></i></a></h4>
+      <table class="tbl-data" border="1" rules="rows">
         <tr>
           <th>NISN</th>
           <th>NAMA</th>
@@ -76,8 +87,8 @@
               <td>$pel[3]</td>
               <td>$pel[4]</td>
               <td>
-                <a href='formedit.php?nisn=".$pel['nisn']."' class=\"button-edit\">Edit</a>      
-                <a href='hapus.php?nisn=".$pel['nisn']."' class=\"button-hapus\">Hapus</a>
+                <a href='formedit.php?nisn=".$pel['nisn']."' class=\"button-edit\">Edit<i class=\"fa-solid fa-pen-to-square\"></i></a>      
+                <a href='hapus.php?nisn=".$pel['nisn']."' class=\"button-hapus\">Hapus<i class=\"fa-solid fa-eraser\"></i></a>
               </td>
           </tr>";
           }
